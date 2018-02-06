d9dd13fd19bffd5f746f94329b5c0b99a4217799
likwokbiu <li_bill@ymail.com>
2018-02-06 09:56:57 -0500

    first commit

---
+++ exercise1.rb
@@ -0,0 +1,54 @@
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  due_date: "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     due_date: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     due_date: "January 1, 2018"
+    },
+    {description: "compile results",
+     due_date: "November 10, 2018"
+    },
+    {description: "version 1",
+     due_date: "January 15, 2019"
+    },
+    {description: "revisions",
+     due_date: "March 30, 2019"
+    },
+    {description: "version 2",
+     due_date: "July 12, 2019"
+    },
+    {description: "final edit",
+     due_date: "October 1, 2019"
+    },
+    {description: "final version",
+     due_date: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     due_date: "December 1, 2019"
+    }
+  ]
+}
+
+puts "Orignal project:-"
+print project
+puts
+puts "===================================================================="
+
+@committee = []
+@committee = project[:committee]
+number_of_committee = @committee.length
+
+i = 0
+project[:steps].each do |step_hash|
+  step_hash[:person] = @committee[i % number_of_committee]
+  i += 1
+end
+
+puts "Updated project:-"
+print project
+puts
